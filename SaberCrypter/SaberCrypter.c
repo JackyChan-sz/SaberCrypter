f891f7e1f4f86f0acdcfd431dca3472a677f7e91
JackyChan-sz <3115549006@qq.com>
2018-01-02 11:42:56 +0800

    Update SaberCrypter.c

--- SaberCrypter/SaberCrypter.c
+++ SaberCrypter/SaberCrypter.c
@@ -1,13 +1,20 @@
 #include "getopt.h"
+#include <stdbool.h>
+
+bool encrypt = false;
 
 int main(int argc, char* argv[])
 {
 	int ch;
-	while ((ch = getopt(argc, argv, "ei:o:f:k")) != -1)
+	while ((ch = getopt(argc, argv, "dei:o:f:k:")) != -1)
 	{
 		switch (ch)
 		{
+		case 'd':
+			encrypt = false;
+			break;
 		case 'e':
+			encrypt = true;
 			break;
 		default:
 			break;
